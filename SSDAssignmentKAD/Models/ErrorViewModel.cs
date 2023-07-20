9ddf731d32fbc46977eed42055d073861cf1bf79
Keeman <Keeman@KEEMAN>
2023-07-20 21:31:39 +0800

    Add project files.

---
+++ SSDAssignmentKAD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSDAssignmentKAD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
